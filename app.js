c6edb6e6ffb10819c2489df2430a1761c9908431
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-01-14 08:59:28 +0530

    used pug template engine for example

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const port = 3000;
 
 const server = app.listen(port,()=>console.log("Server listening on port " + port));
 
-app.get("/",(req,res,next)=>{
-    res.status(200).send("Yahoo");
-});
 
+app.set("view engine","pug");
+app.set("views","views");
+
+
+
+app.get("/",(req,res,next)=>{
+    res.status(200).render("home");
+    //see here we are changing this from "send" to "render"
+});
+//handle get request from the server by default link
 
