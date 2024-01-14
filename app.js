33f8d967261b963d90b8b4aa09f1e7e9d93b6250
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-01-14 08:39:45 +0530

    created a status code and pass cotent

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ const port = 3000;
 
 const server = app.listen(port,()=>console.log("Server listening on port " + port));
 
+app.get("/",(req,res,next)=>{
+    res.status(200).send("Yahoo");
+});
 
 
