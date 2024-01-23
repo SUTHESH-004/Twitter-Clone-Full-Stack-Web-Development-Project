03a7ac6cd4c06ee1cdd3dc53ba3dd3edd87381f7
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-01-23 13:45:53 +0530

    enabling post button only when the text is entered

---
+++ public/js/common.js
@@ -0,0 +1,36 @@
+// $(document).ready(()=>{
+//     // alert("you are ready");
+
+// })
+$("#postTextarea").keyup((event)=>
+{
+    var textbox=$(event.target);
+    var value =textbox.val().trim();
+    console.log(value);
+    var submitbutton =$("#submitPostButton");
+    if(submitbutton.length==0)
+    {
+        return alert("No submit button found");
+    }
+       if(value==""){
+       submitbutton.prop("disabled",true);
+       return;
+       }
+       submitbutton.prop("disabled",false);
+})
+$("#submitPostButton").click((event)=>
+{
+     var button = $(event.target);
+     var textbx =$("#postTextarea");
+
+     var data = {
+        content:textbox.val()
+     }
+     $.post("/api/posts",data,(postdata,status,xhr)=>{
+        
+      
+     }
+    //  xhr xml http request
+    // $.post is a ajax code
+
+ ) })
