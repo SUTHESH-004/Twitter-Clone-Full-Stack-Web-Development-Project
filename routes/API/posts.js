6c202bcbe39ff20c849a9bf53f6c52ab638efff8
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-01-26 17:11:14 +0530

    adding bad request is content does not exits

--- routes/API/posts.js
+++ routes/API/posts.js
@@ -2,16 +2,22 @@ const express = require('express');
 const app =express();
 const router= express.Router();
 const bodyParser = require("body-parser");
-const User = require('../schemas/UserSchema');
+const User = require('../../schemas/UserSchema');
 
 
 
 app.use(bodyParser.urlencoded({extended:false}));
 router.get("/",(req,res,next)=>{
     
-    //see here we are changing this from "send" to "render"
+    
 });
 router.post("/",async(req,res,next)=>{
+
+    if(!req.body.content)
+    {
+        console.log("Content is not valid");
+        return res.sendStatus(400);
+    }
     res.status(200).send("it worked");
    });
  module.exports = router;
