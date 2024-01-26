a478e39b9e8d41e2a86ebd9e2cdab235fd4d3932
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-01-26 17:08:41 +0530

    Creating schema to store values in mango db

---
+++ schemas/PostSchema.js
@@ -0,0 +1,15 @@
+ const mongoose = require('mongoose');
+
+ const Schema = mongoose.Schema;
+
+ const PostSchema = new Schema({
+    content:{type:String,trim:true},
+    postedBy:{type:Schema.Types.ObjectId,ref:'User '},
+    pinned:Boolean
+ },{timestamps: true});
+ // timestamps add created at and updated at in database so it is good have that
+
+
+ var Post = mongoose.model( 'Post', PostSchema );
+ module.exports = PostSchema;
+  
