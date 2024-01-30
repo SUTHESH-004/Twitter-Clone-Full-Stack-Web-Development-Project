5cd40400f1be33d84387ad7925bff00cadc53578
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-01-30 21:00:11 +0530

    looping over all the results in the database & using createpost we create a funtion .

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,22 @@
 $(document).ready(()=>
 {
     alert("it worked");
+      $.get("/api/posts ",(results)=>{
+        // console.log(results);
+        outputPosts(results,$(".postsContainer"))
+
+      })
 })
+
+function outputPosts(results,container){
+        container.html("");
+
+        results.forEach(result=>{
+            var html = createPostHtml(result)
+            container.append(html);
+        });
+        if(result.length==0)
+        {
+            container.append("<span class='noResult'>Nothing to show Create a new ONe<span>");
+        }
+}
