95311cbdb362908c6241c56450b80444921e7027
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-01-30 20:59:09 +0530

    adding the get in posts.js

--- routes/API/posts.js
+++ routes/API/posts.js
@@ -10,7 +10,14 @@ const Post = require('../../schemas/PostSchema');
 
 app.use(bodyParser.urlencoded({extended:false}));
 router.get("/",(req,res,next)=>{
-    
+    Post.find()
+    .then((results)=>{
+       res.status(200).send(results);
+    })
+    .catch(error=>{
+        console.log(error);
+        res.sendStatus(400);
+    })
     
 });
 router.post("/",async( req,res,next)=>{
