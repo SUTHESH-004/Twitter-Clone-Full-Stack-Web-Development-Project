e80fa2c05ebee6dae15b9201775b30c72ecfd230
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-01-31 11:55:22 +0530

    changed the time stamps

--- public/js/common.js
+++ public/js/common.js
@@ -51,7 +51,7 @@ $("#submitPostButton").click((event)=>
         return console.log("User object is not populated");
      }
      var displayName=postedBy.firstName +" "+postedBy.lastName;
-     var timestamp =postedBy.createdAt;
+     var timestamp =timeDifference(new Date(),new Date(postdata.createdAt));
 
     return `<div class="post">
         <div class="mainContentContainer">
@@ -91,3 +91,40 @@ $("#submitPostButton").click((event)=>
     //you can use jquery as well
 
    }
+   function timeDifference(current, previous) {
+
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+
+    var elapsed = current - previous;
+
+    if (elapsed < msPerMinute) {
+        if(elapsed/1000<30)
+         return "Just now"
+        return Math.round(elapsed/1000) + ' seconds ago';   
+    }
+
+    else if (elapsed < msPerHour) {
+         return Math.round(elapsed/msPerMinute) + ' minutes ago';   
+    }
+
+    else if (elapsed < msPerDay ) {
+         return Math.round(elapsed/msPerHour ) + ' hours ago';   
+    }
+
+    else if (elapsed < msPerMonth) {
+        return Math.round(elapsed/msPerDay) + ' days ago';   
+    }
+
+    else if (elapsed < msPerYear) {
+        return Math.round(elapsed/msPerMonth) + ' months ago';   
+    }
+
+    else {
+        return Math.round(elapsed/msPerYear ) + ' years ago';   
+    }
+}
+
